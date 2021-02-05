d4461f1c6e26db943ce3cf866234b0c836f0dc97
Allen Li <supermrli@hotmail.com>
2021-02-06 00:21:27 +0800

    Format navigation buttons for index page

--- miniprogram/page/homePage/index.js
+++ miniprogram/page/homePage/index.js
@@ -17,53 +17,43 @@ Page({
   },
 
   data: {
-    list: [
-      {
-        id: 'view',
-        name: '视图容器',
-        open: false,
-        pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
-      }, {
-        id: 'content',
-        name: '基础内容',
-        open: false,
-        pages: ['text', 'icon', 'progress', 'rich-text']
-      }, {
-        id: 'form',
-        name: '表单组件',
-        open: false,
-        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea', 'editor']
-      }, {
-        id: 'nav',
-        name: '导航',
-        open: false,
-        pages: ['navigator']
-      }, {
-        id: 'media',
-        name: '媒体组件',
-        open: false,
-        pages: ['image', 'video', 'camera', 'live-pusher', 'live-player']
-      }, {
-        id: 'map',
-        name: '地图',
-        open: false,
-        pages: ['map']
-      }, {
-        id: 'canvas',
-        name: '画布',
-        open: false,
-        pages: ['canvas', 'canvas-2d', 'webgl']
-      }, {
-        id: 'open',
-        name: '开放能力',
-        open: false,
-        pages: ['ad', 'open-data', 'web-view']
-      }, {
-        id: 'obstacle-free',
-        name: '无障碍访问',
-        open: false,
-        pages: ['aria-component']
-      }
+    navBtnLst: [
+      [
+        {
+          name: '疫苗接种',
+          page: 'vaccination'
+        },
+        {
+          name: '绝育',
+          page: 'sterilization'
+        },
+        {
+          name: '体检',
+          page: 'physical-examination'
+        },
+        {
+          name: '普通问诊',
+          page: 'general-clinic'
+        },
+      ], 
+      [
+        {
+          name: '办狗证',
+          page: 'dog-license'
+        },
+        {
+          name: '待定',
+          page: 'tt'
+        },
+        {
+          name: '待定',
+          page: 'tt'
+        },
+        {
+          name: '待定',
+          page: 'tt'
+        }
+      ]
     ],
     theme: 'light'
   },
