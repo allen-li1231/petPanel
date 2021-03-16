24b457357e775e435c9e73341718ca2623b24ff4
‘fiiitz.f’ <fiiitz.f@gmail.com>
2021-03-16 20:32:35 +0800

    updated discover pages

--- miniprogram/page/homePage/pages/dog-license/index.js
+++ miniprogram/page/homePage/pages/dog-license/index.js
@@ -1,66 +1,86 @@
 // page/homePage/pages/dog-license/index.js
-Page({
+import CustomPage from '../../../userHome/base/CustomPage'
+const colorLight = 'rgba(0, 0, 0, .9)'
 
-  /**
-   * 页面的初始数据
-   */
+CustomPage({
+  onShareAppMessage() {
+    return {
+      title: '知萌-办狗证',
+      path: 'page/homePage/pages/dog-license/index'
+    }
+  },
   data: {
-
+    selected: 0,
+    lst_hospital_info:[
+      {
+        hospital_name:'医院A',
+        hospital_distance:'x',
+        hospital_tag:'美容；普通外科；狗证'
+      },
+      {
+        hospital_name:'医院B',
+        hospital_distance:'y',
+        hospital_tag:'24小时急诊；狗证；中兽医；牙科'
+      },
+     {
+      hospital_name:'医院C',
+      hospital_distance:'z',
+      hospital_tag:'美容；狗证'
+     },
+    ],
+ navHos:'hosdetail/hosdetail',
+  }
+,
+onLoad:  function(option) {
+  var lst_hospital_info = [
+    {
+      hospital_name:'医院A',
+      hospital_distance:'x',
+      hospital_tag:'美容；普通外科；狗证'
+    },
+    {
+      hospital_name:'医院B',
+      hospital_distance:'y',
+      hospital_tag:'24小时急诊；狗证；中兽医；牙科'
+    },
+   {
+    hospital_name:'医院C',
+    hospital_distance:'z',
+    hospital_tag:'美容；狗证'
+   },
+  ]
+  var value = '狗证';
+  var that = this;
+       
+   // 将标题已关键字拆开成数组    
+   for (let i = 0; i < lst_hospital_info.length; i++) {
+      lst_hospital_info[i].hospital_tag = that.hilight_word(value, lst_hospital_info[i].hospital_tag);
+      this.setData(
+        {
+          'lst_hospital_info': lst_hospital_info
+        }
+      )
+    }
+   
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+   // 根据搜索字分割字符
+  hilight_word: function (key, word) {    
+    let idx = word.indexOf(key), t = [];
+ 
+    if (idx > -1) {
+      if (idx == 0) {
+        t =this.hilight_word(key, word.substr(key.length));
+        t.unshift({ key: true, str: key });
+        return t;
+      }
+ 
+      if (idx > 0) {
+        t =this.hilight_word(key, word.substr(idx));
+        t.unshift({ key: false, str: word.substring(0, idx) });
+        return t;
+      }
+    }
+    return [{ key: false, str: word }];
   }
 })
